90f919cf32a6eb77f2dded58bed5f4262ea391a8
Huong <huongnguyen.nth1709@gmail.com>
2024-03-28 19:57:38 +0200

    define properties of pokemon state and add Card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
 import { useEffect, useState } from 'react';
+import Card from './components/Card';
 
 const App = () => {
   // eslint-disable-next-line no-unused-vars
-  const [data, setData] = useState({});
+  const [pokemon, setPokemon] = useState({
+    name: '',
+    types: [],
+    abilities: [],
+    baseExp: 0,
+    height: 0,
+    weight: 0,
+    url: '',
+  });
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon/ditto')
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => {
+        setPokemon({
+          name: data.name,
+          types: data.types,
+          abilities: data.abilities,
+          baseExp: data.base_experience,
+          height: data.height,
+          weight: data.weight,
+          url: data.sprites.other.home.front_default,
+        });
+      });
   }, []);
-  console.log(data);
 
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Pokemon</h1>
+      <h1 className='text-5xl text-center font-bold font-serif text-blue-900 tracking-widest mt-9 mb-12'>
+        Pokemon
+      </h1>
+      <div className='container mx-auto'>
+        <Card pokemon={pokemon} />
+      </div>
     </div>
   );
 };
