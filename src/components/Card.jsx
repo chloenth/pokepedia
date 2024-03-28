9e02b824e67c71cac268b8381ed6dfea44208e5b
Huong <huongnguyen.nth1709@gmail.com>
2024-03-28 22:38:35 +0200

    modify css height and text-color of Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,10 +13,10 @@ const Card = ({ pokemon }) => {
         src={pokemon.url}
         alt='dittoImage'
       />
-      <div className='info-card  pb-8 text-center'>
+      <div className='info-card h-auto min-h-max pb-8 text-center'>
         <h1 className='text-2xl font-bold py-6'>{pokemon.name}</h1>
-        <p className='mb-2'>
-          Types :{' '}
+        <p className='mb-2 text-emerald-400'>
+          Types :
           {pokemon.types.map((p, i) => (
             <span
               key={i}
@@ -26,21 +26,27 @@ const Card = ({ pokemon }) => {
             </span>
           ))}
         </p>
-        <p className='mb-2'>Base Experience : {pokemon.baseExp}</p>
-        <p className='mb-2'>Height : {pokemon.height}</p>
-        <p className='mb-2'>Weight : {pokemon.weight}</p>
-        <div className='grid grid-cols-3 gap-3 mt-5 px-4 '>
-          <div className='text-start'>
-            Ability
-            <div className='bg-black text-white px-1.5 py-1 mt-1 rounded-sm float-left'>
-              {ability?.ability.name}
-            </div>
+        <p className='mb-2'>
+          <span className='text-emerald-400'>Base Experience :</span> {''}
+          {pokemon.baseExp}
+        </p>
+        <p className='mb-2'>
+          <span className='text-emerald-400'>Height :</span> {pokemon.height}
+        </p>
+        <p className='mb-2'>
+          <span className='text-emerald-400'>Weight :</span> {pokemon.weight}
+        </p>
+        <div className='text-emerald-400'>
+          Ability
+          <div className='bg-black text-white px-1.5 py-1 mt-1 rounded-sm w-max mx-auto '>
+            {ability?.ability.name}
           </div>
-          <div className='col-span-2 text-end'>
-            Hidden Ability
-            <div className='bg-black text-white px-1.5 py-1 mt-1 rounded-sm w-min float-end'>
-              {hiddenAbility?.ability.name}
-            </div>
+        </div>
+
+        <div className='text-emerald-400'>
+          Hidden Ability
+          <div className='bg-black text-white px-1.5 py-1 mt-1 rounded-sm w-max mx-auto'>
+            {hiddenAbility?.ability.name || 'none'}
           </div>
         </div>
       </div>
