ee765ce09ae6a6d685dde08e257c0686a1bc4871
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 11:55:44 +0200

    create SearchBox component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,9 @@
+const SearchBox = () => {
+  return (
+    <div className='p-4'>
+      <input className='p-3' type='search' placeholder='search pokemons' />
+    </div>
+  );
+};
+
+export default SearchBox;
