04042b8aa5c5da3388a91c718f74febc712d45b7
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 16:16:59 +0200

    declare variables by destructuring props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,24 +4,24 @@ import Ability from './card-components/Ability';
 import TextInfo from './card-components/TextInfo';
 
 const Card = ({ pokemon }) => {
-  const ability = pokemon.abilities.find((a) => !a.is_hidden);
-  const hiddenAbility = pokemon.abilities.find((a) => a.is_hidden);
+  const { name, types, abilities, baseExp, height, weight, url } =
+    pokemon && pokemon;
+  const ability = abilities.find((a) => !a.is_hidden);
+  const hiddenAbility = abilities.find((a) => a.is_hidden);
 
   return (
     <div className='w-72 card rounded-xl drop-shadow-2xl inline-block p-1 '>
       <img
         className='w-full h-36 object-contain border-b border-black'
-        src={pokemon.url}
+        src={url}
         alt='dittoImage'
       />
       <div className='info-card h-auto min-h-max pb-8 text-center text-emerald-400'>
-        <h1 className='text-2xl font-bold py-6 text-neutral-400'>
-          {pokemon.name}
-        </h1>
-        <TextInfo label='Types :' info={pokemon.types} />
-        <TextInfo label='Base Experience :' info={pokemon.baseExp} />
-        <TextInfo label='Height :' info={pokemon.height} />
-        <TextInfo label='Weight :' info={pokemon.weight} />
+        <h1 className='text-2xl font-bold py-6 text-neutral-400'>{name}</h1>
+        <TextInfo label='Types :' info={types} />
+        <TextInfo label='Base Experience :' info={baseExp} />
+        <TextInfo label='Height :' info={height} />
+        <TextInfo label='Weight :' info={weight} />
 
         <Ability label='Ability' info={ability?.ability.name} />
 
