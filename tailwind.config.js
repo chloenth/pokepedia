7ced7faa4a86af08e8a89f3ca4cf379821982a2e
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 17:52:06 +0200

    add extra-small break-point

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      xs: '280px',
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
