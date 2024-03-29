fcc2f9987aaf75706014ab90bfdd8f4c0bacd047
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 16:18:12 +0200

    add onChange for input

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,13 @@
-const SearchBox = () => {
+/* eslint-disable react/prop-types */
+const SearchBox = ({ searchChange }) => {
   return (
     <div className='p-4'>
-      <input className='p-3' type='search' placeholder='search pokemons' />
+      <input
+        className='p-3'
+        type='search'
+        placeholder='search pokemons'
+        onChange={searchChange}
+      />
     </div>
   );
 };
