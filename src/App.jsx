109c08dc53a7f5a6ac4a3e9ebff03cafc4de3d6e
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 11:56:24 +0200

    add SearchBox to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import CardList from './components/CardList';
 import Scroll from './components/Scroll';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
   // eslint-disable-next-line no-unused-vars
@@ -33,10 +34,11 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <h1 className='text-5xl text-center font-bold font-serif text-blue-900 tracking-widest mt-9 mb-12'>
+    <div className='text-center'>
+      <h1 className='text-5xl  font-bold font-serif text-blue-900 tracking-widest p-9'>
         Pokemon
       </h1>
+      <SearchBox />
       <Scroll>
         <CardList pokeArray={pokeArray} />
       </Scroll>
