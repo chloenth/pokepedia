36974c67bcaafcfc2445346dc4911d0f7a762579
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 17:55:57 +0200

    make grid layout responsive

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({ pokeArray }) => {
   return (
-    <div className='container mx-auto p-2 grid grid-cols-4 gap-10'>
+    <div className='container mx-auto p-2 grid justify-items-center grid-cols-1 md:grid-cols-2 md:gap-x-0 lg:grid-cols-3 xl:grid-cols-4 gap-y-10'>
       {pokeArray.map((poke) => {
         return <Card key={poke.id} pokemon={poke} />;
       })}
