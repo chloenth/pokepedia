af1f2f46500dd365b09c5941289bc2b75ca6cb1f
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 10:41:03 +0200

    restructure by adding TextInfo and Ability components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,53 +1,34 @@
 /* eslint-disable react/prop-types */
 import '../Card.css';
+import Ability from './card-components/Ability';
+import TextInfo from './card-components/TextInfo';
 
 const Card = ({ pokemon }) => {
   const ability = pokemon.abilities.find((a) => !a.is_hidden);
   const hiddenAbility = pokemon.abilities.find((a) => a.is_hidden);
 
   return (
-    <div className='w-72 card rounded-xl drop-shadow-2xl inline-block p-1 text-neutral-400'>
+    <div className='w-72 card rounded-xl drop-shadow-2xl inline-block p-1 '>
       <img
         className='w-full h-36 object-contain border-b border-black'
         src={pokemon.url}
         alt='dittoImage'
       />
-      <div className='info-card h-auto min-h-max pb-8 text-center'>
-        <h1 className='text-2xl font-bold py-6'>{pokemon.name}</h1>
-        <p className='mb-2 text-emerald-400'>
-          Types :
-          {pokemon.types.map((p, i) => (
-            <span
-              key={i}
-              className='bg-black text-white px-1.5 py-1 m-2 rounded-sm'
-            >
-              {p.type.name}
-            </span>
-          ))}
-        </p>
-        <p className='mb-2'>
-          <span className='text-emerald-400'>Base Experience :</span> {''}
-          {pokemon.baseExp}
-        </p>
-        <p className='mb-2'>
-          <span className='text-emerald-400'>Height :</span> {pokemon.height}
-        </p>
-        <p className='mb-2'>
-          <span className='text-emerald-400'>Weight :</span> {pokemon.weight}
-        </p>
-        <div className='text-emerald-400'>
-          Ability
-          <div className='bg-black text-white px-1.5 py-1 mt-1 rounded-sm w-max mx-auto '>
-            {ability?.ability.name}
-          </div>
-        </div>
+      <div className='info-card h-auto min-h-max pb-8 text-center text-emerald-400'>
+        <h1 className='text-2xl font-bold py-6 text-neutral-400'>
+          {pokemon.name}
+        </h1>
+        <TextInfo label='Types :' info={pokemon.types} />
+        <TextInfo label='Base Experience :' info={pokemon.baseExp} />
+        <TextInfo label='Height :' info={pokemon.height} />
+        <TextInfo label='Weight :' info={pokemon.weight} />
 
-        <div className='text-emerald-400'>
-          Hidden Ability
-          <div className='bg-black text-white px-1.5 py-1 mt-1 rounded-sm w-max mx-auto'>
-            {hiddenAbility?.ability.name || 'none'}
-          </div>
-        </div>
+        <Ability label='Ability' info={ability?.ability.name} />
+
+        <Ability
+          label='Hidden Ability'
+          info={hiddenAbility?.ability.name || 'none'}
+        />
       </div>
     </div>
   );
