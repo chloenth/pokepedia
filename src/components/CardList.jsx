4458d238d868b17a2f81ecb58d0a2325f56fcd70
Huong <huongnguyen.nth1709@gmail.com>
2024-03-29 16:15:16 +0200

    change key-value from index to id when mapping Card component

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,9 +4,9 @@ import Card from './Card';
 const CardList = ({ pokeArray }) => {
   return (
     <div className='container mx-auto p-2 grid grid-cols-4 gap-10'>
-      {pokeArray.map((poke, i) => (
-        <Card key={i} pokemon={poke} />
-      ))}
+      {pokeArray.map((poke) => {
+        return <Card key={poke.id} pokemon={poke} />;
+      })}
     </div>
   );
 };
