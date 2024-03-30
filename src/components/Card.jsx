e10490bb968dfbe67aa2b1a67d95f4d9b23010e3
Huong <huongnguyen.nth1709@gmail.com>
2024-03-30 08:23:51 +0200

    modify class custom bg-image linear-gradient

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,4 @@
 /* eslint-disable react/prop-types */
-import '../Card.css';
 import Ability from './card-components/Ability';
 import TextInfo from './card-components/TextInfo';
 
@@ -10,13 +9,13 @@ const Card = ({ pokemon }) => {
   const hiddenAbility = abilities.find((a) => a.is_hidden);
 
   return (
-    <div className='xs:w-full sm:w-72 card rounded-xl drop-shadow-2xl inline-block p-1 '>
+    <div className='xs:w-full sm:w-72 rounded-xl drop-shadow-2xl inline-block p-1 '>
       <img
-        className='w-full h-36 object-contain border-b border-black'
+        className='w-full h-36 bg-linear-gradient-primary object-contain border-b border-black'
         src={url}
         alt='dittoImage'
       />
-      <div className='info-card h-auto min-h-max pb-8 text-center text-emerald-400'>
+      <div className='bg-linear-gradient-secondary h-auto min-h-max pb-8 text-center text-emerald-400'>
         <h1 className='text-2xl font-bold py-6 text-neutral-400'>{name}</h1>
         <TextInfo label='Types :' info={types} />
         <TextInfo label='Base Experience :' info={baseExp} />
